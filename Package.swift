da9ee98945a714b83d5a63fb492118bcf9c111da
Maarten Wubs <maarten.wubs@icloud.com>
2022-02-10 14:50:02 +0100

    added sources

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PandaKit",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/PandaKit"),
         .testTarget(
             name: "PandaKitTests",
             dependencies: ["PandaKit"]),
