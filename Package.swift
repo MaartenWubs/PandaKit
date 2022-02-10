ff177fed13d97aa1f535b56b8c27c6d2a56fa0a5
Maarten Wubs <maarten.wubs@icloud.com>
2022-02-10 15:04:39 +0100

    some test

--- Package.swift
+++ Package.swift
@@ -8,8 +8,7 @@ let package = Package(
     platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "PandaKit",targets: ["PandaKit"]),
-        .library(name: "PNDAView", type: .static, targets: ["PandaKit"])
+        .library(name: "PandaKit",targets: ["PandaKit"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
--- Sources/PandaKit/PandaKit.swift
+++ Sources/PandaKit/PandaKit.swift
@@ -6,3 +6,17 @@ public class PandaKit {
     }
     
 }
+
+public class PNDAView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+    }
+}
