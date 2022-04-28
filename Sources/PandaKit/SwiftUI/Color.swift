ae4518c1ab58785bb34ea800fff799451fe17038
Maarten Wubs <maarten.wubs@icloud.com>
2022-04-28 20:23:36 +0200

    Start of SwiftUI
    added the start of SwiftUI support of PandaKit. Started by adding the colors in SwiftUI

---
+++ Sources/PandaKit/SwiftUI/Color.swift
@@ -0,0 +1,28 @@
+
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the PandaKit project
+//
+// Copyright (c) 2022 Maarten Wubs
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+import SwiftUI
+
+@available (iOS 15, *)
+extension Color {
+    /// Added templete colors in PandaKit
+    static var cherry       = Color.init(UIColor.cherryColor)
+    static var alizarin     = Color.init(UIColor.alizarinColor)
+    static var papaya       = Color.init(UIColor.papayaColor)
+    static var sunFlower    = Color.init(UIColor.sunFlowerColor)
+    static var nephrits     = Color.init(UIColor.nephritsColor)
+    static var greenSea     = Color.init(UIColor.greenSeaColor)
+    static var amethyst     = Color.init(UIColor.amethystColor)
+    static var wetAsphalt   = Color.init(UIColor.wetAsphaltColor)
+    static var midnightBlue = Color.init(UIColor.midnightBlueColor)
+    static var cloud        = Color.init(UIColor.cloudColor)
+    static var darkCloud    = Color.init(UIColor.darkCloudColor)
+    static var asbestos     = Color.init(UIColor.asbestosColor)
+}
